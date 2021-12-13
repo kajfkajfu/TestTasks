3669eb237cf36344a17b2c3f719a63271be2daae
kajfkajfu <kajfkajfu830@gmail.com>
2021-12-13 20:20:16 +0300

    calc update

---
+++ src/main/java/webCalc/CustomWebDriver.java
@@ -0,0 +1,13 @@
+package webCalc;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CustomWebDriver {
+    protected WebDriver getDriver() {
+        System.setProperty("webdriver.chrome.driver", "D://ChromeDriver//chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://juliemr.github.io/protractor-demo/");
+        return driver;
+    }
+}
--- src/main/java/webCalc/WebCalc.java
+++ src/main/java/webCalc/WebCalc.java
@@ -3,30 +3,33 @@ package webCalc;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebCalc {
+    private final CustomWebDriver webDriver = new CustomWebDriver();
+    private final WebDriver driver = webDriver.getDriver();
+    private final WebElement NUMBER_ONE = driver.findElement(By.xpath("/html/body/div/div/form/input[1]"));
+    private final WebElement NUMBER_TWO = driver.findElement(By.xpath("/html/body/div/div/form/input[2]"));
+    private final WebElement GO_BUTTON = driver.findElement(By.xpath("/html/body/div/div/form/button"));
+    private final WebElement ADDITION_BUTTON = driver.findElement(By.xpath("/html/body/" +
+            "div/div/form/select/option[1]"));
+    private final WebElement DIVIDE_BUTTON = driver.findElement(By.xpath("/html/body/" +
+            "div/div/form/select/option[2]"));
+    private final WebElement MODULO_BUTTON = driver.findElement(By.xpath("/html/body/" +
+            "div/div/form/select/option[3]"));
+    private final WebElement MULTIPLICATION_BUTTON = driver.findElement(By.xpath("/html/body" +
+            "/div/div/form/select/option[4]"));
+    private final WebElement SUBTRACTION_BUTTON = driver.findElement(By.xpath("/html/body" +
+            "/div/div/form/select/option[5]"));
 
-    public void setUp(int firstNumber, int secondNumber) {
-        System.setProperty("webdriver.chrome.driver", "D://ChromeDriver//chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://juliemr.github.io/protractor-demo/");
-        WebElement op1 = driver.findElement(By.xpath("/html/body/div/div/form/input[1]"));
-        WebElement op2 = driver.findElement(By.xpath("/html/body/div/div/form/input[2]"));
-        op1.sendKeys("10.2");
-        op2.sendKeys("5");
-        WebElement goButton = driver.findElement(By.xpath("/html/body/div/div/form/button"));
-        WebElement additionButton = driver.findElement(By.xpath("/html/body/div/div/form/select/option[1]"));
-        WebElement divideButton = driver.findElement(By.xpath("/html/body/div/div/form/select/option[2]"));
-        WebElement moduloButton = driver.findElement(By.xpath("/html/body/div/div/form/select/option[3]"));
-        WebElement multiplicationButton = driver.findElement(By.xpath("/html/body/div/div/form/select/option[4]"));
-        WebElement subtractionButton = driver.findElement(By.xpath("/html/body/div/div/form/select/option[5]"));
-        inputTwoNumbers(firstNumber, secondNumber);
-    }
 
-    public void inputTwoNumbers(int firstNumber, int secondNumber) {
+    public void addition(int firstNumber, int secondNumber) {
         String first = String.valueOf(firstNumber);
-        String second = String.valueOf(firstNumber);
-
+        String second = String.valueOf(secondNumber);
+        NUMBER_ONE.sendKeys(first);
+        NUMBER_TWO.sendKeys(second);
+        ADDITION_BUTTON.click();
+        GO_BUTTON.click();
     }
+
+
 }
